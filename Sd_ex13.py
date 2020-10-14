54001bcc13858b05e28b2213edcbbe7cdd66ccba
Your Name <you@example.com>
2020-10-14 08:30:46 +0100

    Add ex 11 - 13 and Sd for 13 date: 08:32, 14th October, 2020 author: Tallbar Com num 1

---
+++ Sd_ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, Name = argv
+
+height = raw_input("%s, what is your height?" % Name),
+weight = raw_input("%s, what is your weight?" % Name),
+
+print "Thank you for the information."
