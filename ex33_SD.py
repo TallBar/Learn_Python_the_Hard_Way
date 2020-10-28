2e35ea99c3387b640490368e199e5523fe50228e
Your Name <you@example.com>
2020-10-28 03:26:21 +0000

    Adding {29;33} + SD ex 33

---
+++ ex33_SD.py
@@ -0,0 +1,13 @@
+def test(x):
+    i = 0
+    numbers = []
+    while i < x:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i += 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+    print "The numbers: "
+    for num in numbers:
+        print num
